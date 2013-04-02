3076b9b6055f6f2c79c66f5b9718de86d9e4b20c
Renyi Khor <renyi.ace@gmail.com>
2013-04-03 01:22:09 +0800

    Updated setup.py information.

--- setup.py
+++ setup.py
@@ -6,9 +6,11 @@ setup(
     name='mezzanine-themes',
     version='0.3',
     description='Mezzanine CMS themes',
+    long_description=open('README.md').read(),
     author='Renyi Khor',
     author_email='renyi.ace@gmail.com',
+    url='https://github.com/renyi/mezzanine-themes',
     packages=find_packages(),
     include_package_data = True,
-    #install_requires=['mezzanine'],
+    # install_requires=['mezzanine'],
 )
