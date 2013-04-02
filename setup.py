2cf93cad9b79230a6d0ccaed4a1470baf6594db2
Renyi Khor <renyi.ace@gmail.com>
2013-04-03 03:42:59 +0800

    Updated config files.

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
-
 from setuptools import setup, find_packages
 
+NAME = 'mezzanine-themes'
+
+VERSION = '1.0'
+
+DESCRIPTION = """
+Mezzanine CMS themes and plugins.
+"""
+
 setup(
-    name='mezzanine-themes',
-    version='0.3',
-    description='Mezzanine CMS themes',
+    name=NAME,
+    description=DESCRIPTION,
     long_description=open('README.md').read(),
+    version=VERSION,
     author='Renyi Khor',
     author_email='renyi.ace@gmail.com',
     url='https://github.com/renyi/mezzanine-themes',
     packages=find_packages(),
     include_package_data = True,
+    zip_safe=False,
     # install_requires=['mezzanine'],
 )
