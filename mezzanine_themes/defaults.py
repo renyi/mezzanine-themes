66f07b5b963a96d42b6b55b90ebfe3ae3bb9d0c0
Renyi Khor <renyi.ace@gmail.com>
2013-04-03 03:43:28 +0800

    Added defaults.py

---
+++ mezzanine_themes/defaults.py
@@ -0,0 +1,48 @@
+try:
+    from mezzanine.conf import register_setting
+    from django.utils.translation import ugettext as _
+
+    register_setting(
+        name="TEMPLATE_FAV_ICON",
+        description=_("FAV_ICON"),
+        editable=True,
+        default='data:image/x-icon;base64,AAABAAEAEBACAAAAAACwAAAAFgAAACgAAAAQAAAAIAAAAAEAAQAAAAAAQAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AIwBAACCBwAAAeEAAAHBAAA/+QAAI7kAAELtAADBywAAwXkAAMAJAADAsQAA4OMAAGOjAAB+EgAAYA4AAB/8AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA',
+    )
+
+    register_setting(
+        name="TEMPLATE_APPLE_ICON",
+        description=_("APPLE_ICON"),
+        editable=True,
+        default='',
+    )
+
+    register_setting(
+        name="TEMPLATE_USE_CDN",
+        description=_("USE_CDN"),
+        editable=True,
+        default=False,
+    )
+
+    register_setting(
+        name="TEMPLATE_CDN_JQUERY",
+        description=_("JQUERY CDN url"),
+        editable=True,
+        default="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js",
+    )
+
+    register_setting(
+        name="TEMPLATE_CDN_BOOTSTRAP_CSS",
+        description=_("Bootstrap CDN url (css)"),
+        editable=True,
+        default="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css",
+    )
+
+    register_setting(
+        name="TEMPLATE_CDN_BOOTSTRAP_JS",
+        description=_("Bootstrap CDN url (js)"),
+        editable=True,
+        default="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min.js",
+    )
+except ImportError:
+    # You're missing one of Mezzanine's best feature !
+    pass
